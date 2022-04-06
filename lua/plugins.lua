5de2bc79dfd665a5fef5a77585b6d43f3ad58205
Cosine <sak9188@163.com>
2022-04-06 14:07:01 +0800

    提交配置“

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -5,6 +5,9 @@ return require('packer').startup(function()
     use {'dracula/vim', as = 'dracula'}
 
     -- nvim-tree (新增)
+    -- https://zhuanlan.zhihu.com/p/439574087
+    -- https://zhuanlan.zhihu.com/p/438382701
+
     use {
         'kyazdani42/nvim-tree.lua',
         requires = 'kyazdani42/nvim-web-devicons',
